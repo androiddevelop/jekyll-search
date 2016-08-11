597292d8d9ce2ee4806bc56df119a3f2402b0deb
玄恒 <app@codeboy.me>
2016-08-11 19:52:16 +0800

    format resource name

--- search/js/cb-search.js
+++ search/js/cb-search.js
@@ -10,54 +10,54 @@
                 time1 = time2;
                 if (gap < 500) {
                     if (show) {
-                        $(".search-tool").css("display", "none");
+                        $(".cb-search-tool").css("display", "none");
                         show = false;
                     } else {
-                        $(".search-tool").css("display", "block");
+                        $(".cb-search-tool").css("display", "block");
                         show = true;
-                        $("#search-content").val("");
-                        $("#search-content").focus();
+                        $("#cb-search-content").val("");
+                        $("#cb-search-content").focus();
                     }
                     time1 = 0;
                 }
 			}else if(e.keyCode == 27){
-                    $(".search-tool").css("display", "none");
+                    $(".cb-search-tool").css("display", "none");
                     show = false;
                     time1 = 0;
                 }
         });
 
- 		$("#search-content").keyup(function (e) {
+ 		$("#cb-search-content").keyup(function (e) {
             var time2 = new Date().getTime();
             if (window.event.keyCode == 17) {
                 var gap = time2 - time1;
                 time1 = time2;
                 if (gap < 500) {
                     if (show) {
-                        $(".search-tool").css("display", "none");
+                        $(".cb-search-tool").css("display", "none");
                         show = false;
                     } else {
-                        $(".search-tool").css("display", "block");
+                        $(".cb-search-tool").css("display", "block");
                         show = true;
-                        $("#search-content").val("");
-                        $("#search-content").focus();
+                        $("#cb-search-content").val("");
+                        $("#cb-search-content").focus();
                     }
                     time1 = 0;
                 }
             }
         });
 
-        $("#close-btn").click(function () {
-            $(".search-tool").css("display", "none");
+        $("#cb-close-btn").click(function () {
+            $(".cb-search-tool").css("display", "none");
             show = false;
             time1 = 0;
         });
 
-        $("#search-btn").click(function(){
-            $(".search-tool").css("display", "block");
+        $("#cb-search-btn").click(function(){
+            $(".cb-search-tool").css("display", "block");
             show = true;
-            $("#search-content").val("");
-            $("#search-content").focus();
+            $("#cb-search-content").val("");
+            $("#cb-search-content").focus();
             time1 = 0;
         });
 
@@ -69,11 +69,11 @@
                     urls.push(item.url);
                 }
 
-                $("#search-content").typeahead({
+                $("#cb-search-content").typeahead({
                     source: names,
 
                     afterSelect: function (item) {
-						$(".search-tool").css("display", "none");
+						$(".cb-search-tool").css("display", "none");
                         show = false;
                         window.location.href = (urls[names.indexOf(item)]);
                         return item;
